208e19473360b6322d3a6f2b3afc7ba06cf07599
Daniil Kuznetsov <ldan93@yandex.ru>
2023-12-31 17:26:47 +0300

    Update UsersQueueExtension

---
+++ niffler-e-2-e-tests-hw/src/test/java/guru/qa/niffler/test/FriendsTest2.java
@@ -0,0 +1,41 @@
+package guru.qa.niffler.test;
+
+import guru.qa.niffler.jupiter.annotation.User;
+import guru.qa.niffler.jupiter.extension.user.UsersQueueExtension;
+import guru.qa.niffler.model.UserJson;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+
+import static guru.qa.niffler.jupiter.annotation.User.UserType.*;
+
+
+@ExtendWith(UsersQueueExtension.class)
+public class FriendsTest2 {
+
+  @BeforeEach
+  void doLogin(@User(WITH_FRIENDS) UserJson user) {
+//    Selenide.open("http://127.0.0.1:3000/main");
+//    $("a[href*='redirect']").click();
+//    $("input[name='username']").setValue(user.username());
+//    $("input[name='password']").setValue(user.testData().password());
+//    $("button[type='submit']").click();
+  }
+
+  @Test
+  void friendsTableShouldNotBeEmpty0(@User(WITH_FRIENDS) UserJson user, @User(INVITATION_RECIEVED) UserJson user2) throws Exception {
+    UserJson user_1 = user;
+    UserJson user_2 = user2;
+    Thread.sleep(3000);
+  }
+
+  @Test
+  void friendsTableShouldNotBeEmpty1(@User(INVITATION_RECIEVED) UserJson user) throws Exception {
+    Thread.sleep(3000);
+  }
+
+  @Test
+  void friendsTableShouldNotBeEmpty2(@User(WITH_FRIENDS) UserJson user, @User(INVITATION_SEND) UserJson user2) throws Exception {
+    Thread.sleep(3000);
+  }
+}
