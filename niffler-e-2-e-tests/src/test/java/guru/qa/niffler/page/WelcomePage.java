d6e686e2cde6bf3fe4b3f52277abba64bb3442ad
Daniil Kuznetsov <ldan93@yandex.ru>
2024-02-18 18:40:21 +0300

    Up to master

--- niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/WelcomePage.java
+++ niffler-e-2-e-tests/src/test/java/guru/qa/niffler/page/WelcomePage.java
@@ -3,16 +3,33 @@ package guru.qa.niffler.page;
 import com.codeborne.selenide.SelenideElement;
 import io.qameta.allure.Step;
 
+import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.$;
 
-public class WelcomePage {
+public class WelcomePage extends BasePage<WelcomePage> {
 
-    private final SelenideElement loginButton = $("a[href*='redirect']");
-    private final SelenideElement registerButton = $("a[href*='auth']");
+  public static final String URL = CFG.frontUrl();
 
-    @Step("Перейти к странице логина")
-    public LoginPage goToLoginPage() {
-        loginButton.click();
-        return new LoginPage();
-    }
+  private final SelenideElement loginButton = $("a[href*='redirect']");
+  private final SelenideElement registerButton = $("a[href*='register']");
+
+  @Step("Redirect to login page")
+  public LoginPage doLogin() {
+    loginButton.click();
+    return new LoginPage();
+  }
+
+  @Step("Redirect to register page")
+  public RegisterPage doRegister() {
+    registerButton.click();
+    return new RegisterPage();
+  }
+
+  @Step("Check that page is loaded")
+  @Override
+  public WelcomePage waitForPageLoaded() {
+    loginButton.should(visible);
+    registerButton.should(visible);
+    return this;
+  }
 }
